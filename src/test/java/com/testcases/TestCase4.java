4a5cdac49aa8d91501fe1d51de39f6ab2fae9611
Sonal Dhinoja <sonal091186@gmail.com>
2020-03-14 10:25:17 -0400

    add minor change

--- src/test/java/com/testcases/TestCase4.java
+++ src/test/java/com/testcases/TestCase4.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -23,12 +24,18 @@ public class TestCase4 {
 	@Test
 	public void navigatetoGoogle() throws InterruptedException {
 	WebDriver driver = null;
-	WebDriverManager.chromedriver().setup();
-	driver = new ChromeDriver();
-	driver.wait(5000);
+	System.setProperty("webdriver.edge.driver", "C:\\Users\\dhire\\Desktop\\New folder (3)\\edgedriver_win64\\msedgedriver.exe");
+	/*
+	ChromeOptions chromeOptions = new ChromeOptions();
+	chromeOptions.addArguments("--whitelist-ip *");
+	chromeOptions.addArguments("--proxy-server='direct://'");
+	chromeOptions.addArguments("--proxy-bypass-list=*");*/
+	driver = new EdgeDriver();
+	
+//	driver.wait(5000);
 	driver.get("https://www.google.com/");
 	WebElement searchbox = driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[1]/div[1]/div/div[2]/input"));
-	searchbox.sendKeys("what is selenium");		
+	searchbox.sendKeys("what is selenium is used for");
 	}
 
 	/*
